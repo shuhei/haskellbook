049646065d57d125d773d69deb02fc772f806c93
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2017-07-02 21:38:30 +0200

    Chapter 26 Embedded

---
+++ ch26-MonadTransformers/Embedded.hs
@@ -0,0 +1,7 @@
+import Control.Monad.Trans.Except
+import Control.Monad.Trans.Maybe
+import Control.Monad.Trans.Reader
+
+embedded :: MaybeT (ExceptT String (ReaderT () IO)) Int
+embedded =
+  MaybeT . ExceptT . ReaderT $ return <$> const (Right $ Just 1)
